3724f71f9b81e3fe9f1fd4bf831ce0a6931032ac
Jorge Alberto Osuna <jorgealbertocervantes04@gmail.com>
2026-09-01 23:41:24 -0600

    ner

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { 
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip as RechartsTooltip, Legend, ResponsiveContainer,
   PieChart, Pie, Cell
